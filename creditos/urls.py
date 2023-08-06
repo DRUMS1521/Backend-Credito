14aa21d7ed89b35c228d6deda082c69c4a0f157d
cifu1393 <cifu1393@gmail.com>
2023-08-06 03:50:40 -0500

    Update urls.py

--- creditos/urls.py
+++ creditos/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from creditos.views.basics import ListaCreditosView, DetalleCreditoView, CreditoCreateView
 
 urlpatterns = [
-    path('creditos/', ListaCreditosView.as_view(), name='lista_creditos'),
+    path('creditos', ListaCreditosView.as_view(), name='lista_creditos'),
     path('creditos/<int:pk>/', DetalleCreditoView.as_view(), name='detalle_credito'),
-    path('creditos-create/', CreditoCreateView.as_view(), name='create_creditos'),
 ]
