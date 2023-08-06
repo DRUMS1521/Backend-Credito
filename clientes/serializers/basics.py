adde6739d44595ccf13b0258b0733b3e6d812a1c
cifu1393 <cifu1393@gmail.com>
2023-08-05 23:55:14 -0500

    update customers

--- clientes/serializers/basics.py
+++ clientes/serializers/basics.py
@@ -5,6 +5,8 @@ from clientes.models.clientes import Clientes
 
 
 class ClienteSerializer(serializers.ModelSerializer):
+    direccion_2 = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    numero_celular_2 = serializers.CharField(required=False, allow_blank=True, allow_null=True)
     class Meta:
         model = Clientes
         fields = '__all__'
--- clientes/urls.py
+++ clientes/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from clientes.views import ClientesListView, ClienteDetailView, ClienteCreateView
 
 urlpatterns = [
-    path('clientes/', ClientesListView.as_view(), name='lista_clientes'),
-    path('clientes/<int:pk>/', ClienteDetailView.as_view(), name='detalle_cliente'),
-    path('clientes/create/', ClienteCreateView.as_view(), name='crear_cliente'),
+    path('clientes', ClientesListView.as_view(), name='lista_clientes'),
+    path('clientes/<int:pk>', ClienteDetailView.as_view(), name='detalle_cliente'),
+    path('clientes/create', ClienteCreateView.as_view(), name='crear_cliente'),
 
 ]
