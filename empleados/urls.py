9f2c385353dc46ae29c2a3d24acbac3d869d0345
cifu1393 <cifu1393@gmail.com>
2023-08-06 19:30:11 -0500

    Update urls.py

--- empleados/urls.py
+++ empleados/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from empleados.views import EmpleadosListView, EmpleadoDetailView
 
 urlpatterns = [
-    path('empleados/', EmpleadosListView.as_view(), name='lista_empleados'),
-    path('empleados/<int:pk>/', EmpleadoDetailView.as_view(), name='detalle_empleado'),
+    path('empleados', EmpleadosListView.as_view(), name='lista_empleados'),
+    path('empleados/<int:pk>', EmpleadoDetailView.as_view(), name='detalle_empleado'),
 ]
