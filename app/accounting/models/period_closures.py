0ac19ab05bdc001c275dd0977c4e8e6369c4d46a
cifu1393 <cifu1393@gmail.com>
2023-11-23 01:40:39 -0500

    Update period_closures.py

--- app/accounting/models/period_closures.py
+++ app/accounting/models/period_closures.py
@@ -3,6 +3,7 @@ from django.db import models
 class PeriodClosures(models.Model):
     id = models.AutoField(primary_key=True)
     start_date = models.DateField(null=False)
+    
     end_date = models.DateField(null=False)
     closed = models.BooleanField(default=False)
 
