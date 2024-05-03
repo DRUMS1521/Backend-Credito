4a80b4cc2b842207f6eb50f95e0b70aa3a4608d8
cifu1393 <cifu1393@gmail.com>
2024-05-03 12:28:54 -0500

    Update customers.py

--- app/loans/views/customers.py
+++ app/loans/views/customers.py
@@ -12,13 +12,16 @@ class CustomerBasicListAPIView(ListAPIView):
         # get filter params
         name = self.request.query_params.get('name', None)
         phone = self.request.query_params.get('phone', None)
+        document_like = self.request.query_params.get('document_like', None)
         document = self.request.query_params.get('document', None)
         if name is not None and name != '':
             queryset = queryset.filter(name__icontains=name)
         if phone is not None and phone != '':
             queryset = queryset.filter(phone__icontains=phone)
+        if document_like is not None and document_like != '':
+            queryset = queryset.filter(document__icontains=document_like)
         if document is not None and document != '':
-            queryset = queryset.filter(document__icontains=document)
+            queryset = queryset.filter(document=document)
         return queryset
     
 class AllcustomersBasicListAPIView(ListAPIView):
